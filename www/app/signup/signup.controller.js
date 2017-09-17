6cb475f88e0b3cec5ce4d2ca8555f29b8fd72d82
Matthew Victor <vicmcnabby@gmail.com>
2017-09-17 16:40:42 -0600

    added geolocation function

--- www/app/signup/signup.controller.js
+++ www/app/signup/signup.controller.js
@@ -3,7 +3,25 @@
     .module('momento')
     .controller('SignUpController', SignUpController)
 
-  function SignUpController() {
+  function SignUpController($cordovaGeolocation) {
     const vm = this
+
+    vm.getLocation = function() {
+      var posOptions = {
+        timeout: 10000,
+        enableHighAccuracy: false
+      };
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function(position) {
+          console.log(position);
+          var lat = position.coords.latitude
+          var long = position.coords.longitude
+        }, function(err) {
+          // error
+          console.log(err);
+        });
+
+    }
   }
 }());
